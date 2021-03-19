976d55b1e30069a76e35f9e643f8edbb72b1bcec
Yash Raj <yashrj130@gmail.com>
2021-03-19 16:36:35 +0530

    Burger component
    Burger component dynamically outputting ingredients as per state.

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// import Aux from '../../hoc/Auxiliary';
+
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient type={igKey} key={igKey + i} />
+            });
+        });
+
+    // console.log(transformedIngredients);
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+};
+
+export default burger;
