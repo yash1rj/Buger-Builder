293d546cf06f320fb8053ed60a396564e7ff42f8
Yash Raj <yashrj130@gmail.com>
2021-03-19 16:33:02 +0530

    Create BurgerBuilder.js
    Stateful component with Burger and its build control

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+import Aux from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            meat: 2,
+            cheese: 1,
+            salad: 1
+        }
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Burger ingredients={this.state.ingredients} />
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
