22834ce43c79fefdab15452873e289980582e30c
Yash Raj <yashrj130@gmail.com>
2021-03-20 14:18:51 +0530

    Text on when No ingredients added

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -28,8 +28,8 @@ class BurgerIngredient extends Component {
             case ('salad'):
                 ingredient = <div className={classes.Salad}></div>;
                 break;
-            case ('bacon'):
-                ingredient = <div className={classes.Bacon}></div>;
+            case ('paneer'):
+                ingredient = <div className={classes.Paneer}></div>;
                 break;
             default:
                 ingredient = null;
