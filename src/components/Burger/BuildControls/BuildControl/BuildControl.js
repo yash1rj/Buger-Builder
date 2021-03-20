f11d85a958542c196c1d29f5aa9d934d79f9acf2
Yash Raj <yashrj130@gmail.com>
2021-03-20 14:20:10 +0530

    Create BuildControl.js
    add/remove button control component

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,15 @@
+import classes from './BuildControl.module.css';
+import React from 'react';
+
+const buildcontrol = (props) => (
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
+        <button 
+            className={classes.Less} 
+            onClick={props.removed} 
+            disabled={props.disabled}>Less</button>
+        <button className={classes.More} onClick={props.added}>More</button>
+    </div>
+);
+
+export default buildcontrol;
