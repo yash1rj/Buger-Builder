6278a5adfce01452d2e1269eb28ab353b66269dd
Yash Raj <yashrj130@gmail.com>
2021-03-24 12:50:43 +0530

    Showing Total price in Order summary page

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,6 +18,7 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p>Current Price : <strong><span>&#8377;</span>{props.price}</strong></p>
             <p>Continue to checkout ?</p>
             <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
             <Button btnType="Success" clicked={props.purchaseContinued}>PROCEED</Button>
