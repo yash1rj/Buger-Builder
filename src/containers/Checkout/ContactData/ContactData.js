b4fce98915df11451022e261e6b3192d296c789e
Yash Raj <yashrj130@gmail.com>
2021-05-01 22:39:31 +0530

    Using the dynamic input element in Contact data form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -5,6 +5,7 @@ import Button from '../../../components/UI/Button/Button';
 
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
@@ -55,10 +56,10 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <input className={classes.Input} type='text' name="name" placeholder="Name" />
-                <input className={classes.Input} type='email' name="email" placeholder="Email" />
-                <input className={classes.Input} type='text' name="street" placeholder="Street" />
-                <input className={classes.Input} type='text' name="zipCode" placeholder="Zip Code" />
+                <Input inputtype="input" type='text' name="name" placeholder="Name" />
+                <Input inputtype="input" type='email' name="email" placeholder="Email" />
+                <Input inputtype="input" type='text' name="street" placeholder="Street" />
+                <Input inputtype="input" type='text' name="zipCode" placeholder="Zip Code" />
 
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
