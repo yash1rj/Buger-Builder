5c033b4318ff7371f73d77f02c09b6945d629a10
Yash Raj <yashrj130@gmail.com>
2021-05-01 18:48:50 +0530

    Update Checkout.js
    Parsing ingredients from query params adn rendering checkout summary and ContactData form on order continuation

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import ContactData from "./ContactData/ContactData";
 
 
 class Checkout extends Component {
@@ -12,9 +14,40 @@ class Checkout extends Component {
         }
     }
 
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        for (let param of query.entries()) {
+            // ['salad' , '1']
+            ingredients[param[0]] = +param[1];
+        }
+
+        this.setState({
+            ingredients: ingredients
+        });
+    }
+
+    checkoutCancelledHandler = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinuedHandler = () => {
+        this.props.history.replace('/checkout/contact-data');
+    }
+
     render() {
         return (
-            <CheckoutSummary ingredients={this.state.ingredients} />
+            <div>
+                <CheckoutSummary
+                    ingredients={this.state.ingredients}
+                    checkoutCancelled={this.checkoutCancelledHandler}
+                    checkoutContinued={this.checkoutContinuedHandler} />
+
+                <Route path={this.props.match.path + '/contact-data'}
+                    render={() => (
+                        <ContactData ingredients={this.state.ingredients} />
+                    )} />
+            </div>
         );
     }
 }
