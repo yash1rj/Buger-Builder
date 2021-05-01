7eced170a445db3e0a400890905e5e7e99c9e1ab
Yash Raj <yashrj130@gmail.com>
2021-05-01 20:54:15 +0530

    Update Order.js
    -rendering ingredients and totalprice in formatted manner
    -styling singular order

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,34 @@
 import classes from './Order.module.css';
 import React from 'react';
 
-const order = () => (
-    <div className={classes.Order}>
-        <p>Ingredients : Salad (1)</p>
-        <p>Price : <strong><span>&#8377;</span>15</strong></p>
-    </div>
-);
+const order = (props) => {
+    const ingredients = [];
+
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]
+        });
+    }
+
+    const ingredientOutput = ingredients.map(ig => {
+        return <span 
+            key={ig.name}
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #DAA972',
+                padding: '5px'
+            }}>{ig.name} ({ig.amount})</span>;
+    });
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients : {ingredientOutput}</p>
+            <p>Price : <strong><span>&#8377;</span>{props.price}</strong></p>
+        </div>
+    );
+}
 
 export default order;
