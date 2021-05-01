1bee373d730a76197067b185b8db5a5ca2769b75
Yash Raj <yashrj130@gmail.com>
2021-05-01 20:28:40 +0530

    Update App.js
    Add route for Orders page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/" exact component={BurgerBuilder} />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
