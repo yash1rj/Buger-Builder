ee81f9bfb03878646a6326cc692baf92037cb494
Yash Raj <yashrj130@gmail.com>
2021-05-01 19:58:06 +0530

    Update BurgerBuilder.js
    Sending totslPrice as  part of query params

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -48,43 +48,16 @@ class BurgerBuilder extends Component {
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+
+        //also send totalPrice in query params
+        queryParams.push('price=' + this.state.totalPrice);
+
         const queryString=queryParams.join('&');
 
         this.props.history.push({
             pathname: '/checkout',
             search: '?' + queryString
         });
-
-        // this.setState({ loading: true });
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Yash Raj',
-        //         address: {
-        //             street: 'cda street',
-        //             zipCode: '80023',
-        //             country: 'India'
-        //         },
-        //         email: 'yash@test.com',
-        //         deliveryMethod: 'fastest'
-        //     }
-        // };
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         // console.log(response);
-        //         this.setState({
-        //             loading: false,
-        //             purchasing: false
-        //         });
-        //     })
-        //     .catch(err => {
-        //         // console.log(err);
-        //         this.setState({
-        //             loading: false,
-        //             purchasing: false
-        //         });
-        //     });
     
     }
 
