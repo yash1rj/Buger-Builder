d3683e75302927aed521b78a3cf4f4fbf8af0f34
Yash Raj <yashrj130@gmail.com>
2021-05-02 11:30:59 +0530

    Update Input.js
    -updating inputElement based on JS config used in state

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,15 +4,39 @@ import React from 'react';
 const input = (props) => {
     let inputElement = null;
 
-    switch(props.inputtype) {
-        case('input') :
-            inputElement = <input className={classes.InputElement} {...props} />;
+    switch (props.elementType) {
+        case ('input'):
+            inputElement = <input
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onChange= {props.changed} />;
             break;
-        case('textarea') :
-            inputElement = <textarea className={classes.InputElement} {...props} />;
+        case ('textarea'):
+            inputElement = <textarea
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onChange= {props.changed} />;
             break;
-        default :
-            inputElement = <input className={classes.InputElement} {...props} />;
+        case ('select'):
+            inputElement = (
+                <select
+                    className={classes.InputElement}
+                    onChange= {props.changed}
+                    value={props.value}>
+                        {props.elementConfig.options.map(option => (
+                            <option key={option.value} value={option.value}>{option.displayName}</option>
+                        ))}
+                </select>
+            );
+            break;
+        default:
+            inputElement = <input
+                className={classes.InputElement}
+                onChange= {props.changed}
+                {...props.elementConfig}
+                value={props.value} />;
     }
 
     return (
