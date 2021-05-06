f781478a7e228cd8c5ab7963fb9ba95e94566403
Yash Raj <yashrj130@gmail.com>
2021-05-06 17:58:51 +0530

    Splitting up actionCreators and Reducers

---
+++ src/store/reducers/burgerBuilder.js
@@ -0,0 +1,57 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 15,
+    error: false
+};
+
+const INGREDIENTS_PRICES = {
+    salad: 10,
+    meat: 20,
+    cheese: 12,
+    paneer: 15
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                },
+                totalPrice: state.totalPrice + INGREDIENTS_PRICES[action.ingredientName]
+            };
+        case actionTypes.REMOVE_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+                },
+                totalPrice: state.totalPrice - INGREDIENTS_PRICES[action.ingredientName]
+            };
+        case actionTypes.SET_INGREDIENTS:
+            return {
+                ...state,
+                ingredients: {
+                    salad: action.ingredients.salad,
+                    meat: action.ingredients.meat,
+                    cheese: action.ingredients.cheese,
+                    paneer: action.ingredients.paneer
+                },
+                error: false
+            };
+        case actionTypes.FETCH_INGREDIENTS_FAILED:
+            return {
+                ...state,
+                error: true
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
