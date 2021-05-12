ac349e10c5531a353b8ba2309ef263b97fa16370
Yash Raj <yashrj130@gmail.com>
2021-05-12 11:39:16 +0530

    Update Orders.js
    -mapping reducer state and disptach function to props
    -removing/commenting state related code and using reducer state and methods
    -showing a spinner while orders view page is loaded/ data fetched from direbase

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -3,48 +3,65 @@ import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions/index';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
-    state = {
-        orders: [],
-        loading: true
-    }
 
     componentDidMount() {
-        axios.get('/orders.json')
-            .then(res => {
-                // console.log(res.data);
-                const fetchedOrders = [];
-                for (let key in res.data) {
-                    fetchedOrders.push({
-                        ...res.data[key],
-                        id: key
-                    });
-                }
-                this.setState({
-                    loading: false,
-                    orders: fetchedOrders
-                });
-            })
-            .catch(err => {
-                this.setState({
-                    loading: false
-                });
-            })
+        // axios.get('/orders.json')
+        //     .then(res => {
+        //         // console.log(res.data);
+        //         const fetchedOrders = [];
+        //         for (let key in res.data) {
+        //             fetchedOrders.push({
+        //                 ...res.data[key],
+        //                 id: key
+        //             });
+        //         }
+        //         this.setState({
+        //             loading: false,
+        //             orders: fetchedOrders
+        //         });
+        //     })
+        //     .catch(err => {
+        //         this.setState({
+        //             loading: false
+        //         });
+        //     })
+        this.props.onFetchOrders();
     }
 
     render() {
-        return (
-            <div>
-                {this.state.orders.map(order => (
-                    <Order 
-                        key={order.id}
-                        ingredients={order.ingredients}
-                        price={order.price} />
-                ))}
-            </div>
-        );
+        let allOrders = <Spinner />;
+        if(!this.props.loading) {
+            allOrders = (
+                <div>
+                    {this.props.orders.map(order => (
+                        <Order
+                            key={order.id}
+                            ingredients={order.ingredients}
+                            price={order.price} />
+                    ))}
+                </div>
+            );
+        }
+        return allOrders;
     }
 }
 
-export default withErrorHandler(Orders, axios);
+const mapStateToProps = state => {
+    return {
+        orders: state.orders.orders,
+        loading: state.orders.loading
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onFetchOrders: () => dispatch(actions.fetchOrders())
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
