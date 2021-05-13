2f3582126d27b78c5ccc38f412b9bde408550cf6
Yash Raj <yashrj130@gmail.com>
2021-05-13 23:09:38 +0530

    Update Auth.js
    using loading state from reducer and showing a spinner on authentication start

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.module.css';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 import * as actions from '../../store/actions/auth';
 import { connect } from 'react-redux';
@@ -108,7 +109,7 @@ class Auth extends Component {
             });
         }
 
-        const form = formElementsArray.map(formElement => (
+        let form = formElementsArray.map(formElement => (
             <Input
                 key={formElement.id}
                 elementType={formElement.config.elementType}
@@ -120,6 +121,10 @@ class Auth extends Component {
                 valueType={formElement.id}
                 changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ));
+
+        if (this.props.loading) {
+            form = <Spinner />;
+        }
 
         return (
             <div className={classes.Auth}>
@@ -135,10 +140,16 @@ class Auth extends Component {
     }
 }
 
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading
+    };
+};
+
 const mapDispatchToProps = dispatch => {
     return {
         onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
     };
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
