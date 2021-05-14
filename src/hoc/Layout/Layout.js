a386368ad6b5cac0def6c8231dc981dc83850734
Yash Raj <yashrj130@gmail.com>
2021-05-14 15:14:14 +0530

    Modifying UI depending on auth state
    -show authenticated or logout navigation link depending on user token

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Aux from '../Auxiliary/Auxiliary';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import { connect } from 'react-redux';
 
 import classes from './Layout.module.css';
 
@@ -23,8 +24,11 @@ class Layout extends Component {
     render() {
         return (
             <Aux>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <Toolbar 
+                    isAuth={this.props.isAuthenticated}
+                    drawerToggleClicked={this.sideDrawerToggleHandler} />
                 <SideDrawer 
+                    isAuth={this.props.isAuthenticated}
                     open={this.state.showSideDrawer} 
                     closed={this.sideDrawerClosedHandler} />
                 <main className={classes.Content}>
@@ -35,4 +39,10 @@ class Layout extends Component {
     }
 }
 
-export default Layout;
+const mapStateToProps = (state) => {
+    return {
+        isAuthenticated: state.auth.tokenId !== null
+    };
+};
+
+export default connect(mapStateToProps)(Layout);
