0a69da55a6f31c4dbca9df785e33ea702a59f546
Yash Raj <yashrj130@gmail.com>
2021-05-16 15:45:37 +0530

    Update auth.js
    -new redirectpath redux status added
    -setting up action for settingRedirectpath status

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -5,7 +5,8 @@ const initialState = {
     tokenId: null,
     userId: null,
     error: null,
-    loading: false
+    loading: false,
+    authRedirectPath: '/'
 };
 
 const authStart = (state, action) => {
@@ -38,12 +39,19 @@ const authLogout = (state, action) => {
     })
 };
 
+const authSetRedirectPath = (state, action) => {
+    return updateObject(state, {
+        authRedirectPath: action.path
+    });
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.AUTH_START: return authStart(state, action);
         case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
         case actionTypes.AUTH_FAIL: return authFail(state, action);
         case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
+        case actionTypes.AUTH_SET_REDIRECT_PATH: return authSetRedirectPath(state, action);
         default: return state;
     }
 };
