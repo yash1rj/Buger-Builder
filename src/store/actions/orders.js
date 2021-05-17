039b3c41aa355563ac3af15949681fd9305ba926
Yash Raj <yashrj130@gmail.com>
2021-05-17 18:27:02 +0530

    Update orders.js
    -updating the query params for fetching order for only fetching order of logged inn user

--- src/store/actions/orders.js
+++ src/store/actions/orders.js
@@ -64,10 +64,11 @@ export const fetchOrderStart = () => {
     };
 };
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrderStart());
-        axios.get('/orders.json?auth=' + token)
+        const queryParams =  '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+        axios.get('/orders.json' + queryParams)
             .then(res => {
                 // console.log(res.data);
                 const fetchedOrders = [];
