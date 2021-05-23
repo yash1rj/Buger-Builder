f3073c2bcff41a08411875138acbc5c4cbad2d79
Yash Raj <yashrj130@gmail.com>
2021-05-23 08:47:53 +0530

    Update Modal.js
    -converting class based component to functional component with hooks
    -using React.memo instead of shouldComponentUpdate

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,29 +1,37 @@
 import classes from './Modal.module.css';
-import React, { Component } from 'react';
+import React from 'react';
 
 import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-class Modal extends Component {
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-    }
+const Modal = props => {
 
-    render() {
-        return (
-            <Aux>
-                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-                <div
-                    className={classes.Modal}
-                    style={{
-                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                        opacity: this.props.show ? '1' : '0'
-                    }}>
-                    {this.props.children}
-                </div>
-            </Aux>
-        );
-    }
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return nextProps.show !== props.show || nextProps.children !== props.children;
+    // }
+    // Instead we can use React memo
+    // And if we only want to check a subset of props and not all before updating, 
+    // React memo takes a second argument(function) where we can add our own comparison
+    // this fn. receives prevProps and nextProps as input then we return true/false 
+    // to deceide whether the props are equal or not
+    // opposite logic of what we have in shouldComponentUpdate
+
+    return (
+        <Aux>
+            <Backdrop show={props.show} clicked={props.modalClosed} />
+            <div
+                className={classes.Modal}
+                style={{
+                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }}>
+                {props.children}
+            </div>
+        </Aux>
+    );
 }
 
-export default Modal;
+export default React.memo(Modal, (prevProps, nextProps) => 
+    nextProps.show == prevProps.show && 
+    nextProps.children == prevProps.children
+);
